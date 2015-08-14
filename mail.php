8d0197f09991fffce6bc917e9f12860806d00c83
vpolyanko <vpolyanko@gmail.com>
2015-08-13 17:38:13 -0700

    Return Link
    Link updated

--- mail.php
+++ mail.php
@@ -6,5 +6,5 @@ $recipient = "vpolyanko@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!";
 ?>
